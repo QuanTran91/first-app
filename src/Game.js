3bde84b8ef99c2b1bfbd0e58b2d961030f15c572
Quan (Quant) H. TRAN <quant@enclave.vn>
2017-06-02 16:56:19 +0700

    Foo bar

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+class GameDemo extends Component{
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
+
+
 class Game extends Component{
     render(){
         return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
+global.jQuery = require('jquery');
+require('bootstrap');
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
